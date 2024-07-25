f33a9266b0823e56444461a27eab05d63615b79f
Kerrick Cavanaugh <kerrickcavanaugh@Kerricks-MacBook-Pro.local>
2024-07-25 19:29:19 -0400

    asdf

--- tests/testUtils.js
+++ tests/testUtils.js
@@ -9,9 +9,7 @@ const invalidServerUrl =
   "https://invalid.execute-api.us-east-2.amazonaws.com/not_real";
 
 const adminKey = process.env.ADMIN_API_KEY;
-// const adminKey = "IIhgYuScAuztZbWK54km38yc0da9him3Q3wyCuQ3";
 const regKey = process.env.REG_API_KEY;
-// const regKey = "IIhgYuScAuztZbWK54km38yc0da9him3Q3wyCuQ3";
 const invalidKey = "invalidApiKey123";
 
 const originType = "Integration Tests";
