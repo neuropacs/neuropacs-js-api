3cbad5c23a7557be0729fc6f85e8502c40ef4383
Kerrick Cavanaugh <kerrickcavanaugh@Kerricks-MacBook-Pro.local>
2024-07-25 19:22:59 -0400

    integration tests

--- tests/neuropacs.test.js
+++ tests/neuropacs.test.js
@@ -1,62 +1,45 @@
-/**
-- invalid server url
-- generate aes key
-    - success X
-- get public key
-    - success X
-- connect 
-    - success X
-    - invalid api key X
-- new job 
-    - success X
-    - invalid connection id X
-- upload 
-    - File object success 
-    - Uint8Array success 
-- upload dataset 
-    - dataset path success 
-- run job
-    - success
-    - invalid product 
-    - invalid order id 
-    - invalid connection id 
-- check status
-    - success 
-    - invalid order id 
-    - invalid connection id 
-- get results
-    - success 
-        - TXT 
-        - JSON 
-        - XML 
-    - invalid result format 
-    - invalid order id 
-    - invalid connection id
-**/
-
 require("../src/neuropacs.js");
-
-const adminKey = "cdXVNIFzEUbSElTpoVoK4SyRrJ7Zj6n6Y6wgApIc";
-const regKey = "7PRHFkrxE71dpBNGw2HaS8PxesOzrZZB2XEWU3Xj";
+const {
+  isValidUuid4,
+  isValidSessionObj,
+  readDirectory,
+  isValidStatusObj,
+  devServerUrl,
+  adminKey,
+  originType,
+  regKey,
+  invalidKey,
+  invalidServerUrl
+} = require("./testUtils");
+const path = require("path");
 
 const npcsAdmin = Neuropacs.init({
-  serverUrl: "https://sl3tkzp9ve.execute-api.us-east-2.amazonaws.com/dev",
-  apiKey: adminKey
+  serverUrl: devServerUrl,
+  apiKey: adminKey,
+  originType: originType
 });
 
 const npcsReg = Neuropacs.init({
-  serverUrl: "https://sl3tkzp9ve.execute-api.us-east-2.amazonaws.com/dev",
-  apiKey: regKey
+  serverUrl: devServerUrl,
+  apiKey: regKey,
+  originType: originType
 });
 
-const npcsInvalid = Neuropacs.init({
-  serverUrl: "https://invalid.execute-api.us-east-2.amazonaws.com/not_real",
-  apiKey: regKey
+const npcsInvalidApiKey = Neuropacs.init({
+  serverUrl: devServerUrl,
+  apiKey: invalidKey,
+  originType: originType
+});
+
+const npcsInvalidServerUrl = Neuropacs.init({
+  serverUrl: invalidServerUrl,
+  apiKey: regKey,
+  originType: originType
 });
 
 // Invalid server URL
 test("invalid server URL", async () => {
-  await expect(await npcsInvalid.connect()).rejects.toThrow(
+  await expect(npcsInvalidServerUrl.connect()).rejects.toThrow(
     "Connection creation failed: OAEP encryption failed: Retrieval of public key failed: request to https://invalid.execute-api.us-east-2.amazonaws.com/not_real/api/getPubKey/ failed, reason: getaddrinfo ENOTFOUND invalid.execute-api.us-east-2.amazonaws.com"
   );
 });
@@ -64,6 +47,78 @@ test("invalid server URL", async () => {
 // Successful connection
 test("successful connection", async () => {
   const session = await npcsAdmin.connect();
-  console.log(session);
-  expect(typeof session).toBe("object");
+  expect(isValidSessionObj(session)).toBe(true);
 });
+
+// Invalid API key
+test("invalid api key", async function () {
+  await expect(npcsInvalidApiKey.connect()).rejects.toThrow(
+    "Connection creation failed: API key not found."
+  );
+});
+
+// Successful order creation
+test("successful order creation", async () => {
+  await npcsAdmin.connect();
+  const orderId = await npcsAdmin.newJob();
+  expect(isValidUuid4(orderId)).toBe(true);
+});
+
+// Test no connection id in request header
+test("missing connection id in request header", async function () {
+  npcsReg.connectionId = "";
+  await expect(npcsReg.newJob()).rejects.toThrow(
+    "Job creation failed: No connection ID in request header."
+  );
+});
+
+// Successful dataset upload
+// test("successful dataset upload", async () => {
+//   await npcsAdmin.connect();
+//   const orderId = await npcsAdmin.newJob();
+//   console.log(orderId);
+//   const dataset = await readDirectory("./tests/test_dataset");
+//   console.log(dataset);
+//   const uploadStatus = await npcsAdmin.uploadDataset({
+//     dataset: dataset,
+//     orderId: orderId,
+//     datasetId: orderId,
+//     callback: (info) => {
+//       console.log(info);
+//     }
+//   });
+//   console.log(uploadStatus);
+// });
+
+// Successful status check
+test("successful status check", async () => {
+  await npcsAdmin.connect();
+  const status = await npcsAdmin.checkStatus({ orderId: "TEST" });
+  expect(isValidStatusObj(status)).toBe(true);
+});
+
+// Invalid order Id
+test("Invalid order id in status check", async function () {
+  await npcsAdmin.connect();
+  await expect(npcsAdmin.checkStatus({ orderId: "NotValid" })).rejects.toThrow(
+    "Check job status failed: Bucket not found."
+  );
+});
+
+// // Successful result retrieval in raw txt format
+// test("successful result retrieval", async () => {
+//   await npcsAdmin.connect();
+//   const status = await npcsAdmin.getResults({
+//     orderId: "TEST",
+//     format: "TXT",
+//     dataType: "raw"
+//   });
+//   expect(isValidStatusObj(status)).toBe(true);
+// });
+
+// // Invalid result format
+// test("invalid result format", async () => {
+//   await npcsAdmin.connect();
+//   const status = await npcsAdmin.getResults({ orderId: "TEST" });
+//   expect(isValidStatusObj(status)).toBe(true);
+// });
---
+++ tests/testUtils.js
@@ -0,0 +1,134 @@
+const { validate: uuidValidate, version: uuidVersion } = require("uuid");
+const base64 = require("base-64");
+const fs = require("fs").promises;
+const path = require("path");
+
+const devServerUrl =
+  "https://sl3tkzp9ve.execute-api.us-east-2.amazonaws.com/dev";
+const invalidServerUrl =
+  "https://invalid.execute-api.us-east-2.amazonaws.com/not_real";
+
+// const adminKey = process.env.ADMIN_API_KEY;
+const adminKey = "IIhgYuScAuztZbWK54km38yc0da9him3Q3wyCuQ3";
+// const regKey = process.env.REG_API_KEY;
+const regKey = "IIhgYuScAuztZbWK54km38yc0da9him3Q3wyCuQ3";
+const invalidKey = "invalidApiKey123";
+
+const originType = "Integration Tests";
+
+const isValidUuid4 = (value) => {
+  return uuidValidate(value) && uuidVersion(value) === 4;
+};
+
+const isObject = (value) => {
+  return value && typeof value === "object" && value.constructor === Object;
+};
+
+const isValidAesCtrKey = (key) => {
+  try {
+    const decodedKey = base64.decode(key);
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
+
+const isValidTimestamp = (timestamp) => {
+  const date = new Date(timestamp);
+  return date instanceof Date && !isNaN(date);
+};
+
+const isValidSessionObj = (conn) => {
+  if (
+    conn &&
+    isValidTimestamp(conn.timestamp) &&
+    isValidUuid4(conn.connectionId) &&
+    isValidAesCtrKey(conn.aesKey)
+  )
+    return true;
+
+  return false;
+};
+
+const readDirectory = async (dirPath) => {
+  try {
+    const files = await fs.readdir(dirPath);
+    const fileObjects = [];
+
+    for (const file of files) {
+      const filePath = path.join(dirPath, file);
+      const stats = await fs.stat(filePath);
+
+      fileObjects.push({
+        name: file,
+        path: filePath,
+        isFile: stats.isFile(),
+        isDirectory: stats.isDirectory(),
+        size: stats.size,
+        mtime: stats.mtime
+      });
+    }
+
+    return fileObjects;
+  } catch (error) {
+    console.error("Error reading directory:", error);
+    throw error;
+  }
+};
+
+const isValidStatusObj = (statusObj) => {
+  if (
+    statusObj &&
+    isObject(statusObj) &&
+    typeof statusObj.started == "boolean" &&
+    typeof statusObj.finished == "boolean" &&
+    typeof statusObj.failed == "boolean" &&
+    statusObj.progress &&
+    typeof statusObj.progress == "number" &&
+    statusObj.info &&
+    typeof statusObj.info == "string"
+  )
+    return true;
+
+  return false;
+};
+
+const isValidResultRawXml = (result) => {};
+
+const isValidResultRawJson = (result) => {};
+
+const isValidResultRawPng = (result) => {};
+
+const isValidResultRawTxt = (result) => {};
+
+const isValidResultBlobTxt = (result) => {};
+
+const isValidResultBlobXml = (result) => {};
+
+const isValidResultBlobJson = (result) => {};
+
+const isValidResultBlobPng = (result) => {};
+
+module.exports = {
+  isValidAesCtrKey,
+  isObject,
+  isValidUuid4,
+  isValidTimestamp,
+  readDirectory,
+  isValidStatusObj,
+  isValidResultRawTxt,
+  isValidResultBlobTxt,
+  isValidResultRawXml,
+  isValidResultBlobXml,
+  isValidResultRawJson,
+  isValidResultBlobJson,
+  isValidResultRawPng,
+  isValidResultBlobPng,
+  isValidSessionObj,
+  devServerUrl,
+  invalidKey,
+  invalidServerUrl,
+  adminKey,
+  regKey,
+  originType
+};
